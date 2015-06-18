ba44e4f19e323ab98041f47d4d3de4d45e03b4a2
Chelsea Liu <cl314@duke.edu>
2015-06-18 08:57:32 -0400

    Initial Commit

---
+++ FastestRPM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastestRPM
+//
+//  Created by Chelsea Liu on 6/18/15.
+//  Copyright (c) 2015 Chelsea Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
